0b14173ab25ce1b71fe7a971f4eeffaf77685df4
Issan Lee <issan.lee.il@gmail.com>
2022-11-12 19:55:45 +0800

    Login and Register Page

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
 import { ThemeProvider } from "styled-components/native";
+import * as firebase from "firebase";
 
 import {
   useFonts as useOswald,
@@ -11,8 +12,20 @@ import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 import { theme } from "./src/infrastructure/theme";
 import { Navigation } from "./src/infrastructure/navigation";
 
-import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context";
-import { LocationContextProvider } from "./src/services/location/location.context";
+import { AuthenticationContextProvider } from "./src/services/authentication/authentication.context";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCvn-7r3yle3m-lcOZ_2O6cWTDp9SKsTXY",
+  authDomain: "ifood-ad4af.firebaseapp.com",
+  projectId: "ifood-ad4af",
+  storageBucket: "ifood-ad4af.appspot.com",
+  messagingSenderId: "428142018302",
+  appId: "1:428142018302:web:65ffa46f264cc7ff572faf"
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
@@ -30,11 +43,9 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <LocationContextProvider>
-          <RestaurantsContextProvider>
-            <Navigation />
-          </RestaurantsContextProvider>
-        </LocationContextProvider>
+        <AuthenticationContextProvider>
+          <Navigation />
+        </AuthenticationContextProvider>
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
