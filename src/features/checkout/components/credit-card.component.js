74918ee5690dc2b200dba3b10d5a6adc72bbf6ba
Issan Lee <issan.lee.il@gmail.com>
2022-11-12 20:01:52 +0800

    Cart page

--- src/features/checkout/components/credit-card.component.js
+++ src/features/checkout/components/credit-card.component.js
@@ -1,12 +1,12 @@
-import React from "react";
-import { LiteCreditCardInput } from "react-native-credit-card-input";
+//import React from "react";
+//import { LiteCreditCardInput } from "react-native-credit-card-input";
 
-export const CreditCardInput = () => {
-  const onChange = (formData) => {
-    const { values, status } = formData;
-    const isIncomplete = Object.values(status).includes("incomplete");
-    console.log(isIncomplete);
-  };
-  return <LiteCreditCardInput onChange={onChange} />;
-};
+//export const CreditCardInput = () => {
+  //const onChange = (formData) => {
+  //  const { values, status } = formData;
+  //  const isIncomplete = Object.values(status).includes("incomplete");
+  //  console.log(isIncomplete);
+ // };
+//  return <LiteCreditCardInput onChange={onChange} />;
+//};
   
