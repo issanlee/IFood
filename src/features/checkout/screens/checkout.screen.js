8a4dd03b597b598149bda46ce6703db4064e2d26
Issan Lee <issan.lee.il@gmail.com>
2022-11-12 20:03:15 +0800

    App Theme Clean up

--- src/features/checkout/screens/checkout.screen.js
+++ src/features/checkout/screens/checkout.screen.js
@@ -1,11 +1,68 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { ScrollView } from "react-native";
+import { List } from "react-native-paper";
 
+import { Text } from "../../../components/typography/text.component";
+import { Spacer } from "../../../components/spacer/spacer.component";
 import { SafeArea } from "../../../components/utility/safe-area.component";
 
-import { CreditCardInput } from "../components/credit-card.component";
+import { CartContext } from "../../../services/cart/cart.context";
 
-export const CheckoutScreen = () => (
-  <SafeArea>
-    <CreditCardInput />
-  </SafeArea>
-);
+import { CartIconContainer, CartIcon, PayButton, ClearButton } from "../components/checkout.styles";
+import { RestaurantInfoCard } from "../../restaurants/components/restaurant-info-card.component";
+
+export const CheckoutScreen = ({ navigation }) => {
+  const { cart, restaurant, clearCart, sum } = useContext(CartContext);
+
+  if (!cart.length || !restaurant) {
+    return (
+      <SafeArea>
+        <CartIconContainer>
+          <CartIcon icon="cart-off" />
+          <Text>Your cart is empty!</Text>
+        </CartIconContainer>
+      </SafeArea>
+    );
+  }
+  return (
+    <SafeArea>
+      <RestaurantInfoCard restaurant={restaurant} />
+      <ScrollView>
+        <Spacer position="left" size="medium">
+          <Spacer position="top" size="large">
+            <Text>Your Order</Text>
+          </Spacer>
+          <List.Section>
+            {cart.map(({ item, price }) => {
+              return <List.Item title={`${item} - ${price / 100}`} />;
+            })}
+          </List.Section>
+          <Text>Total: {sum / 100}</Text>
+        </Spacer>
+
+        <Spacer position="top" size="large">        
+        </Spacer>
+        <Spacer position="top" size="xxl" />
+
+        <PayButton
+          //icon="cash-usd"
+          mode="contained"
+          
+          onPress={() => { clearCart(),navigation.navigate("CheckoutSuccess")
+          }
+        }
+        >
+          Pay
+        </PayButton>   
+
+        <Spacer position="top" size="large">
+
+          <ClearButton icon="cart-off" mode="contained" onPress={clearCart}>
+            Clear Cart
+          </ClearButton>
+          </Spacer>
+
+      </ScrollView>
+    </SafeArea>
+  );
+};
